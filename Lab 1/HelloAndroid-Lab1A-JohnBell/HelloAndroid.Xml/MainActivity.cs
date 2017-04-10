47aa0e0549a1b26e4d87368e252b31a9dac0e03e
John Bell <insidmal@insidmal.com>
2017-04-10 14:16:04 -0700

    beta

---
+++ Lab 1/HelloAndroid-Lab1A-JohnBell/HelloAndroid.Xml/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid.Xml
+{
+    [Activity(Label = "HelloAndroid.Xml", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+
+
+            /*
+            base.OnCreate(bundle);
+            var layout = new LinearLayout(this);
+
+            layout.Orientation = Orientation.Vertical;
+
+            var aLabel = new TextView(this);
+            aLabel.SetText(Resource.String.helloLabelText);
+
+            var aButton = new Button(this);
+            aButton.SetText(Resource.String.helloButtonText);
+            aButton.Click += (sender, e) =>
+            {
+                aLabel.Text = "Hello from the button";
+            };
+
+
+            layout.AddView(aLabel);
+            layout.AddView(aButton);
+            SetContentView(layout);
+    */
+
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.aLabel);
+            var resetButton = FindViewById<Button>(Resource.Id.resetButton);
+
+
+            aButton.Click += (sender, e) =>
+            {
+                aLabel.Text = "Hello from the button";
+            };
+
+            resetButton.Click += (sender, e) =>
+            {
+                aLabel.SetText(Resource.String.helloLabelText);
+            };
+
+        }
+    }
+}
+
---
+++ Lab 1/HelloAndroid-Lab1A-JohnBell/HelloAndroid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var layout = new LinearLayout(this);
+
+            layout.Orientation = Orientation.Vertical;
+
+            var aLabel = new TextView(this);
+            aLabel.SetText(Resource.String.helloLabelText);
+
+            var aButton = new Button(this);
+            aButton.SetText(Resource.String.helloButtonText);
+            aButton.Click += (sender, e) =>
+            {
+                aLabel.Text = "Hello from the button";
+            };
+
+            var resetButton = new Button(this);
+            resetButton.SetText(Resource.String.resetButtonText);
+            resetButton.Click += (sender, e) => {
+                aLabel.SetText(Resource.String.helloLabelText);
+            };
+
+            layout.AddView(aButton);
+            layout.AddView(aLabel);
+            layout.AddView(resetButton);
+            SetContentView(layout);
+        }
+    }
+}
+
