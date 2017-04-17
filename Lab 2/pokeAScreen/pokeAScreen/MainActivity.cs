59713512466ff811cda964aba9aaf7e560220180
John Bell <insidmal@insidmal.com>
2017-04-17 10:51:39 -0700

    lab 2b beta1

---
+++ Lab 2/pokeAScreen/pokeAScreen/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace pokeAScreen
+{
+    [Activity(Label = "pokeAScreen", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
+            var firstPoke = FindViewById<Button>(Resource.Id.firstPokeButton);
+            firstPoke.Click += (sender, e) => {
+                var second = new Intent(this, typeof(SecondActivity));
+                second.PutExtra("poke", true);
+                SetContentView(second);
+
+            };
+
+            var firstHi = FindViewById<Button>(Resource.Id.firstHiButton);
+            firstHi.Click += (sender, e) => {
+                var second = new Intent(this, typeof(SecondActivity));
+                second.PutExtra("text", "Hi from screen 1");
+                SetContentView(second);
+            };
+        }
+    }
+}
+
---
+++ Lab 2/pokeAScreen/pokeAScreen/SecondActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace pokeAScreen
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var label = FindViewById<TextView>(Resource.Id.secondText);
+            if (savedInstanceState.GetBoolean("poke")) label.Text = "Poked by Screen 1!";
+            else if (savedInstanceState.GetString("text") != null) label.Text = savedInstanceState.GetString("text");
+            else label.Text = "Nothing sent from screen one :(";
+        }
+    }
+}
